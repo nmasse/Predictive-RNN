8ca76b7fe17b7e362ecf286fc4cfbce29507f593
Nicolas Masse <nicolas.masse@gmail.com>
2018-11-02 16:16:34 -0500

    major changes, model running

--- model.py
+++ model.py
@@ -23,12 +23,11 @@ class Model:
 
     """ RNN model for supervised and reinforcement learning training """
 
-    def __init__(self, input_data, target_data, mask, gating):
+    def __init__(self, input_data, target_data, mask):
 
         # Load input activity, target data, training mask, etc.
         self.input_data         = tf.unstack(input_data, axis=0)
         self.target_data        = tf.unstack(target_data, axis=0)
-        self.gating             = tf.reshape(gating, [1,-1])
         self.time_mask          = tf.unstack(mask, axis=0)
 
         # Declare all Tensorflow variables
@@ -45,33 +44,24 @@ class Model:
         """ Initialize all required variables """
 
         # All the possible prefixes based on network setup
-        lstm_var_prefixes   = ['Wf', 'Wi', 'Wo', 'Wc', 'Uf', 'Ui', 'Uo', 'Uc', 'bf', 'bi', 'bo', 'bc']
-        bio_var_prefixes    = ['W_in', 'b_rnn', 'W_rnn']
+        lstm_var_prefixes   = ['Wf', 'Wi', 'Wo', 'Wc', 'Uf', 'Ui', 'Uo', 'Uc', 'bf', 'bi', 'bo', 'bc', 'W_pred', 'b_pred']
         rl_var_prefixes     = ['W_pol_out', 'b_pol_out', 'W_val_out', 'b_val_out']
-        base_var_prefies    = ['W_out', 'b_out']
+        #base_var_prefies    = ['W_out', 'b_out']
 
         # Add relevant prefixes to variable declaration
-        prefix_list = base_var_prefies
-        if par['architecture'] == 'LSTM':
-            prefix_list += lstm_var_prefixes
-        elif par['architecture'] == 'BIO':
-            prefix_list += bio_var_prefixes
-
-        if par['training_method'] == 'RL':
-            prefix_list += rl_var_prefixes
-        elif par['training_method'] == 'SL':
-            pass
+        prefix_list = []
+        prefix_list += lstm_var_prefixes
+        prefix_list += rl_var_prefixes
 
         # Use prefix list to declare required variables and place them in a dict
         self.var_dict = {}
         with tf.variable_scope('network'):
-            for p in prefix_list:
-                self.var_dict[p] = tf.get_variable(p, initializer=par[p+'_init'])
-
-        if par['architecture'] == 'BIO':
-            # Modify recurrent weights if using EI neurons (in a BIO architecture)
-            self.W_rnn_eff = (tf.constant(par['EI_matrix']) @ tf.nn.relu(self.var_dict['W_rnn'])) \
-                if par['EI'] else self.var_dict['W_rnn']
+            for name in lstm_var_prefixes:
+                self.var_dict[name] = []
+                for i in range(par['num_pred_cells']):
+                    self.var_dict[name].append(tf.get_variable(name + str(i), initializer = par[name + '_init'][i]))
+            for name in rl_var_prefixes:
+                self.var_dict[name] = tf.get_variable(name, initializer = par[name + '_init'])
 
 
     def rnn_cell_loop(self):
@@ -82,116 +72,88 @@ class Model:
         self.output = []
         self.mask = []
         self.mask.append(tf.constant(np.ones((par['batch_size'], 1), dtype = np.float32)))
-        if par['training_method'] == 'RL':
-            self.pol_out = self.output  # For interchangeable use
-            self.val_out = []
-            self.action = []
-            self.reward = []
-            self.reward.append(tf.constant(np.zeros((par['batch_size'], par['n_val']), dtype = np.float32)))
+        self.pol_out = self.output  # For interchangeable use
+        self.val_out = []
+        self.action = []
+        self.reward = []
+        reward = tf.constant(np.zeros((par['batch_size'], par['n_val']), dtype = np.float32))
+        self.reward.append(reward)
 
         # Initialize state records
         self.h      = []
-        #self.syn_x  = []
-        #self.syn_u  = []
+        self.total_pred_error = [[] for _ in range(par['num_pred_cells'])]
 
         # Initialize network state
-        if par['architecture'] == 'BIO':
-            h = self.gating*tf.constant(par['h_init'])
-            c = tf.constant(par['h_init'])
-        elif par['architecture'] == 'LSTM':
-            h = tf.zeros_like(par['h_init'])
-            c = tf.zeros_like(par['h_init'])
-        syn_x = tf.constant(par['syn_x_init'])
-        syn_u = tf.constant(par['syn_u_init'])
+        h = [tf.zeros_like(par['h_init'][i]) for i in range(par['num_pred_cells'])]
+        c = [tf.zeros_like(par['h_init'][i]) for i in range(par['num_pred_cells'])]
+
         mask  = self.mask[0]
 
         # Loop through the neural inputs, indexed in time
-        for rnn_input, target, time_mask in zip(self.input_data, self.target_data, self.time_mask):
+        for stimulus_input, target, time_mask in zip(self.input_data, self.target_data, self.time_mask):
 
-            # Compute the state of the hidden layer
-            h, c, syn_x, syn_u = self.recurrent_cell(h, c, syn_x, syn_u, rnn_input)
+            # we're assuming that predictive cells are arranged in a sequence
+            for i in range(par['num_pred_cells']):
+                # Compute the state of the hidden layer
+                # x is the input into the cell, y is the top-down activity projecting to cell
+                y = None if i == par['num_pred_cells']-1 else h[i+1]
+                x = stimulus_input if i == 0 else h[i-1]
+                x = tf.concat([x, reward], axis = -1)
+                h[i], c[i], error_signal = self.predictive_cell(x, y, h[i], c[i], i)
+                self.total_pred_error[i].append(tf.reduce_mean(error_signal)/par['num_pred_cells']/par['num_time_steps'])
 
-            # Record hidden state
+            # Compute outputs for action
+            pol_out        = h[-1] @ self.var_dict['W_pol_out'] + self.var_dict['b_pol_out']
+            action_index   = tf.multinomial(pol_out, 1)
+            action         = tf.one_hot(tf.squeeze(action_index), par['n_pol'])
+
+            # Compute outputs for loss
+            pol_out        = tf.nn.softmax(pol_out, 1)  # Note softmax for entropy loss
+            val_out        = h[-1] @ self.var_dict['W_val_out'] + self.var_dict['b_val_out']
+
+            # Check for trial continuation (ends if previous reward was non-zero)
+            continue_trial = tf.cast(tf.equal(self.reward[-1], 0.), tf.float32)
+            mask          *= continue_trial
+            reward         = tf.reduce_sum(action*target, axis=1, keep_dims=True)*mask*tf.reshape(time_mask,[par['batch_size'], 1])
+
+            # Record RL outputs
+            self.pol_out.append(pol_out)
+            self.val_out.append(val_out)
+            self.action.append(action)
+            self.reward.append(reward)
             self.h.append(h)
-            #self.syn_x.append(syn_x)
-            #self.syn_u.append(syn_u)
-
-            if par['training_method'] == 'SL':
-                # Compute outputs for loss
-                y = h @ self.var_dict['W_out'] + self.var_dict['b_out']
-
-                # Record supervised outputs
-                self.output.append(y)
-
-            elif par['training_method'] == 'RL':
-                # Compute outputs for action
-                pol_out        = h @ self.var_dict['W_pol_out'] + self.var_dict['b_pol_out']
-                action_index   = tf.multinomial(pol_out, 1)
-                action         = tf.one_hot(tf.squeeze(action_index), par['n_pol'])
-
-                # Compute outputs for loss
-                pol_out        = tf.nn.softmax(pol_out, 1)  # Note softmax for entropy loss
-                val_out        = h @ self.var_dict['W_val_out'] + self.var_dict['b_val_out']
-
-                # Check for trial continuation (ends if previous reward was non-zero)
-                continue_trial = tf.cast(tf.equal(self.reward[-1], 0.), tf.float32)
-                mask          *= continue_trial
-                reward         = tf.reduce_sum(action*target, axis=1, keep_dims=True)*mask*tf.reshape(time_mask,[par['batch_size'], 1])
-
-                # Record RL outputs
-                self.pol_out.append(pol_out)
-                self.val_out.append(val_out)
-                self.action.append(action)
-                self.reward.append(reward)
 
             # Record mask (outside if statement for cross-comptability)
             self.mask.append(mask)
 
         # Reward and mask trimming where necessary
         self.mask = self.mask[1:]
-        if par['training_method'] == 'RL':
-            self.reward = self.reward[1:]
+        self.reward = self.reward[1:]
 
 
-    def recurrent_cell(self, h, c, syn_x, syn_u, rnn_input):
+    def predictive_cell(self, x, y, h, c, cell_num):
         """ Using the appropriate recurrent cell
             architecture, compute the hidden state """
 
-        if par['architecture'] == 'BIO':
+        pos_err = tf.nn.relu(x - h @ self.var_dict['W_pred'][cell_num] - self.var_dict['b_pred'][cell_num])
+        neg_err = tf.nn.relu(h @ self.var_dict['W_pred'][cell_num] + self.var_dict['b_pred'][cell_num] - x)
+        error_signal = tf.concat([pos_err, neg_err], axis = -1)
+        rnn_input = error_signal if y is None else tf.concat([error_signal, y], axis = -1)
 
-            # Apply synaptic short-term facilitation and depression, if required
-            if par['synapse_config'] == 'std_stf':
-                syn_x += par['alpha_std']*(1-syn_x) - par['dt_sec']*syn_u*syn_x*h
-                syn_u += par['alpha_stf']*(par['U']-syn_u) + par['dt_sec']*par['U']*(1-syn_u)*h
-                syn_x = tf.minimum(np.float32(1), tf.nn.relu(syn_x))
-                syn_u = tf.minimum(np.float32(1), tf.nn.relu(syn_u))
-                h_post = syn_u*syn_x*h
-            else:
-                h_post = h
+        # Compute LSTM state
+        # f : forgetting gate, i : input gate,
+        # c : cell state, o : output gate
+        f   = tf.sigmoid(rnn_input @ self.var_dict['Wf'][cell_num] + h @ self.var_dict['Uf'][cell_num] + self.var_dict['bf'][cell_num])
+        i   = tf.sigmoid(rnn_input @ self.var_dict['Wi'][cell_num] + h @ self.var_dict['Ui'][cell_num] + self.var_dict['bi'][cell_num])
+        cn  = tf.tanh(rnn_input @ self.var_dict['Wc'][cell_num] + h @ self.var_dict['Uc'][cell_num] + self.var_dict['bc'][cell_num])
+        c   = f * c + i * cn
+        o   = tf.sigmoid(rnn_input @ self.var_dict['Wo'][cell_num] + h @ self.var_dict['Uo'][cell_num] + self.var_dict['bo'][cell_num])
 
-            # Compute hidden state
-            h = self.gating*tf.nn.relu((1-par['alpha_neuron'])*h \
-              + par['alpha_neuron']*(rnn_input @ self.var_dict['W_in'] + h_post @ self.W_rnn_eff + self.var_dict['b_rnn']) \
-              + tf.random_normal(h.shape, 0, par['noise_rnn'], dtype=tf.float32))
-            c = tf.constant(-1.)
+        # Compute hidden state
+        h = o * tf.tanh(c)
 
-        elif par['architecture'] == 'LSTM':
 
-            # Compute LSTM state
-            # f : forgetting gate, i : input gate,
-            # c : cell state, o : output gate
-            f   = tf.sigmoid(rnn_input @ self.var_dict['Wf'] + h @ self.var_dict['Uf'] + self.var_dict['bf'])
-            i   = tf.sigmoid(rnn_input @ self.var_dict['Wi'] + h @ self.var_dict['Ui'] + self.var_dict['bi'])
-            cn  = tf.tanh(rnn_input @ self.var_dict['Wc'] + h @ self.var_dict['Uc'] + self.var_dict['bc'])
-            c   = f * c + i * cn
-            o   = tf.sigmoid(rnn_input @ self.var_dict['Wo'] + h @ self.var_dict['Uo'] + self.var_dict['bo'])
-
-            # Compute hidden state
-            h = self.gating * o * tf.tanh(c)
-            syn_x = tf.constant(-1.)
-            syn_u = tf.constant(-1.)
-
-        return h, c, syn_x, syn_u
+        return h, c, error_signal
 
 
     def optimize(self):
@@ -273,8 +235,11 @@ class Model:
             # Entropy loss
             self.entropy_loss = -par['entropy_cost']*tf.reduce_mean(tf.reduce_sum(mask_static*self.time_mask*self.pol_out*tf.log(epsilon+self.pol_out), axis=1))
 
+            # Prediction loss
+            self.pred_loss = par['error_cost'] * tf.reduce_mean(self.total_pred_error)
+
             # Collect RL losses
-            RL_loss = self.pol_loss + self.val_loss - self.entropy_loss
+            RL_loss = self.pol_loss + self.val_loss - self.entropy_loss + self.pred_loss
 
         # Collect loss terms and compute gradients
         total_loss = sup_loss + RL_loss + self.aux_loss + self.spike_loss
@@ -549,8 +514,7 @@ def reinforcement_learning(save_fn='test.pkl', gpu_id=None):
     tf.reset_default_graph()
 
     # Define all placeholders
-    x, target, mask, pred_val, actual_action, \
-        advantage, mask, gating = generate_placeholders()
+    x, target, mask, pred_val, actual_action, advantage, mask = generate_placeholders()
 
     # Set up stimulus and accuracy recording
     stim = stimulus.MultiStimulus()
@@ -570,7 +534,7 @@ def reinforcement_learning(save_fn='test.pkl', gpu_id=None):
         device = '/cpu:0' if gpu_id is None else '/gpu:0'
         with tf.device(device):
             # Check order against args unpacking in model if editing
-            model = Model(x, target, mask, gating)
+            model = Model(x, target, mask)
 
         # Initialize variables and start the timer
         sess.run(tf.global_variables_initializer())
@@ -589,20 +553,20 @@ def reinforcement_learning(save_fn='test.pkl', gpu_id=None):
                 mk = mk[...,np.newaxis]
 
                 # Put together the feed dictionary
-                feed_dict = {x:input_data, target:reward_data, mask:mk, gating:par['gating'][task]}
+                feed_dict = {x:input_data, target:reward_data, mask:mk}
 
                 # Calculate and apply gradients
                 if par['stabilization'] == 'pathint':
-                    _, _, _, pol_loss, val_loss, aux_loss, spike_loss, ent_loss, h_list, reward_list = \
+                    _, _, _, pol_loss, val_loss, aux_loss, spike_loss, ent_loss, pred_loss, h_list, reward_list = \
                         sess.run([model.train_op, model.update_current_reward, model.update_small_omega, model.pol_loss, model.val_loss, \
-                        model.aux_loss, model.spike_loss, model.entropy_loss, model.h, model.reward], feed_dict = feed_dict)
+                        model.aux_loss, model.spike_loss, model.entropy_loss, model.pred_loss, model.h, model.reward], feed_dict = feed_dict)
                     if i>0:
                         sess.run([model.update_small_omega])
                     sess.run([model.update_previous_reward])
                 elif par['stabilization'] == 'EWC':
-                    _, _, pol_loss,val_loss, aux_loss, spike_loss, ent_loss, h_list, reward_list = \
+                    _, _, pol_loss,val_loss, aux_loss, spike_loss, ent_loss, pred_loss, h_list, reward_list = \
                         sess.run([model.train_op, model.update_current_reward, model.pol_loss, model.val_loss, \
-                        model.aux_loss, model.spike_loss, model.entropy_loss, model.h, model.reward], feed_dict = feed_dict)
+                        model.aux_loss, model.spike_loss, model.entropy_loss, model.pred_loss, model.h, model.reward], feed_dict = feed_dict)
 
                 # Record accuracies
                 reward = np.stack(reward_list)
@@ -614,8 +578,8 @@ def reinforcement_learning(save_fn='test.pkl', gpu_id=None):
                         break
 
                 # Display network performance
-                if i%500 == 0:
-                    print('Iter ', i, 'Task name ', name, ' accuracy', acc, ' aux loss', aux_loss, \
+                if i%200 == 0:
+                    print('Iter ', i, 'Task name ', name, ' accuracy', acc, ' aux loss', aux_loss, ' pred loss', pred_loss, \
                     'mean h', np.mean(np.stack(h_list)), 'time ', np.around(time.time() - task_start_time))
 
             # Update big omegaes, and reset other values before starting new task
@@ -730,14 +694,13 @@ def append_model_performance(model_performance, reward, entropy_loss, pol_loss, 
 def generate_placeholders():
 
     mask = tf.placeholder(tf.float32, shape=[par['num_time_steps'], par['batch_size'], 1])
-    x = tf.placeholder(tf.float32, shape=[par['num_time_steps'], par['batch_size'], par['n_input']])  # input data
+    x = tf.placeholder(tf.float32, shape=[par['num_time_steps'], par['batch_size'], par['n_cell_input'][0]-1])  # input data
     target = tf.placeholder(tf.float32, shape=[par['num_time_steps'], par['batch_size'], par['n_pol']])  # input data
     pred_val = tf.placeholder(tf.float32, shape=[par['num_time_steps'], par['batch_size'], par['n_val'], ])
     actual_action = tf.placeholder(tf.float32, shape=[par['num_time_steps'], par['batch_size'], par['n_pol']])
     advantage  = tf.placeholder(tf.float32, shape=[par['num_time_steps'], par['batch_size'], par['n_val']])
-    gating = tf.placeholder(tf.float32, [par['n_hidden']], 'gating')
 
-    return x, target, mask, pred_val, actual_action, advantage, mask, gating
+    return x, target, mask, pred_val, actual_action, advantage, mask
 
 
 def main(save_fn='testing', gpu_id=None):
--- parameters.py
+++ parameters.py
@@ -22,19 +22,19 @@ par = {
     'exc_inh_prop'          : 0.8,          # Literature 0.8, for EI off 1
     'var_delay'             : False,
     'training_method'       : 'RL',         # 'SL', 'RL'
-    'architecture'          : 'BIO',       # 'BIO', 'LSTM'
+    'architecture'          : 'LSTM',       # 'BIO', 'LSTM'
 
     # Network shape
-    'num_motion_tuned'      : 64,
+    'num_motion_tuned'      : 48,
     'num_fix_tuned'         : 4,
     'num_rule_tuned'        : 0,
-    'n_hidden'              : 256,
     'n_val'                 : 1,
     'include_rule_signal'   : True,
+    'n_hidden'              : [32, 32],
 
     # Timings and rates
     'dt'                    : 20,
-    'learning_rate'         : 1e-3,
+    'learning_rate'         : 4e-3,
     'membrane_time_constant': 100,
     'connection_prob'       : 1.0,
     'discount_rate'         : 0.,
@@ -57,10 +57,11 @@ par = {
     'tuning_height'         : 4.0,        # magnitude scaling factor for von Mises
 
     # Cost values
-    'spike_cost'            : 1e-7,
+    'spike_cost'            : 0.,
     'weight_cost'           : 0.,
     'entropy_cost'          : 0.0001,
     'val_cost'              : 0.01,
+    'error_cost'            : 0.1,
 
     # Synaptic plasticity specs
     'tau_fast'              : 200,
@@ -118,35 +119,28 @@ def update_dependencies():
     ### Putting together network structure
     ###
 
-    # Turn excitatory-inhibitory settings on or off
-    if par['architecture'] == 'BIO':
-        par['EI'] = True if par['exc_inh_prop'] < 1 else False
-    elif par['architecture'] == 'LSTM':
-        print('Using LSTM networks; setting to EI to False')
-        par['EI'] = False
-        par['exc_inh_prop'] = 1.
-        par['synapse_config'] = None
-        par['spike_cost'] = 0.
-
-    # Generate EI matrix
-    par['num_exc_units'] = int(np.round(par['n_hidden']*par['exc_inh_prop']))
-    par['num_inh_units'] = par['n_hidden'] - par['num_exc_units']
-    par['EI_list'] = np.ones(par['n_hidden'], dtype=np.float32)
-    if par['EI']:
-        n = par['n_hidden']//par['num_inh_units']
-        par['ind_inh'] = np.arange(n-1,par['n_hidden'],n)
-        par['EI_list'][par['ind_inh']] = -1.
-    par['EI_matrix'] = np.diag(par['EI_list'])
+    print('Using LSTM networks; setting to EI to False')
+    par['EI'] = False
+    par['exc_inh_prop'] = 1.
+    par['synapse_config'] = None
+    par['spike_cost'] = 0.
 
     # Number of output neurons
     par['n_output'] = par['num_motion_dirs'] + 1
     par['n_pol'] = par['num_motion_dirs'] + 1
 
     # Number of input neurons
-    par['n_input'] = par['num_motion_tuned'] + par['num_fix_tuned'] + par['num_rule_tuned']
+    par['num_pred_cells'] = len(par['n_hidden'])
+    par['n_cell_input'] = []
+    par['n_LSTM_input'] = []
+    # input into predictive cell will consist of feedforward input plus "dopamine" reward signal
+    par['n_cell_input'].append((par['num_motion_tuned'] + par['num_fix_tuned'] + par['num_rule_tuned'] + 1))
+    for i in range(1, par['num_pred_cells']):
+        par['n_cell_input'].append((par['n_hidden'][i-1] + 1))
 
-    # General network shape
-    par['shape'] = (par['n_input'], par['n_hidden'], par['n_output'])
+    for i in range(par['num_pred_cells']-1):
+        par['n_LSTM_input'].append(2*par['n_cell_input'][i] + par['n_hidden'][i-1])
+    par['n_LSTM_input'].append(2*par['n_cell_input'][-1])
 
     # Specify time step in seconds and neuron time constant
     par['dt_sec'] = par['dt']/1000
@@ -160,124 +154,43 @@ def update_dependencies():
     par['num_time_steps'] = par['multistim_trial_length']//par['dt']
 
     # Set up gating vectors for hidden layer
-    gen_gating()
+    #gen_gating()
 
     ###
     ### Setting up weights, biases, masks, etc.
     ###
 
     # Specify initial RNN state
-    par['h_init'] = 0.1*np.ones((par['batch_size'], par['n_hidden']), dtype=np.float32)
+    par['h_init'] = []
+    for i in range(par['num_pred_cells']):
+        par['h_init'].append(0.1*np.ones((par['batch_size'], par['n_hidden'][i]), dtype=np.float32))
 
     # Initialize weights
     c = 0.05
 
-    par['W_in_init'] = c*np.float32(np.random.gamma(shape=0.25, scale=1.0, size = [par['n_input'], par['n_hidden']]))
-    par['W_out_init'] = np.float32(np.random.uniform(-c, c, size = [par['n_hidden'], par['n_output']]))
-
-    if par['EI']:
-        par['W_rnn_init'] = c*np.float32(np.random.gamma(shape=0.25, scale=1.0, size = [par['n_hidden'], par['n_hidden']]))
-        par['W_rnn_mask'] = np.ones((par['n_hidden'], par['n_hidden']), dtype=np.float32) - np.eye(par['n_hidden'])
-        par['W_rnn_init'] *= par['W_rnn_mask']
-    else:
-        par['W_rnn_init'] = np.float32(np.random.uniform(-c, c, size = [par['n_hidden'], par['n_hidden']]))
-        par['W_rnn_mask'] = np.ones((par['n_hidden'], par['n_hidden']), dtype=np.float32)
-
-    # Initialize biases
-    par['b_rnn_init'] = np.zeros((1,par['n_hidden']), dtype = np.float32)
-    par['b_out_init'] = np.zeros((1,par['n_output']), dtype = np.float32)
-
-    # Specify masks
-    par['W_out_mask'] = np.ones((par['n_hidden'], par['n_output']), dtype=np.float32)
-    par['W_in_mask'] = np.ones((par['n_input'], par['n_hidden']), dtype=np.float32)
-    if par['EI']:
-        par['W_out_init'][par['ind_inh'], :] = 0
-        par['W_out_mask'][par['ind_inh'], :] = 0
-
     # Initialize RL-specific weights
-    par['W_pol_out_init'] = np.float32(np.random.uniform(-c, c, size = [par['n_hidden'], par['n_pol']]))
+    par['W_pol_out_init'] = np.float32(np.random.uniform(-c, c, size = [par['n_hidden'][-1], par['n_pol']]))
     par['b_pol_out_init'] = np.zeros((1,par['n_pol']), dtype = np.float32)
 
-    par['W_val_out_init'] = np.float32(np.random.uniform(-c, c, size = [par['n_hidden'], par['n_val']]))
+    par['W_val_out_init'] = np.float32(np.random.uniform(-c, c, size = [par['n_hidden'][-1], par['n_val']]))
     par['b_val_out_init'] = np.zeros((1,par['n_val']), dtype = np.float32)
 
-    ###
-    ### Setting up LSTM weights and biases, if required
-    ###
-
-    if par['architecture'] == 'LSTM':
-        c = 0.05
-        par['Wf_init'] =  np.float32(np.random.uniform(-c, c, size = [par['n_input'], par['n_hidden']]))
-        par['Wi_init'] =  np.float32(np.random.uniform(-c, c, size = [par['n_input'], par['n_hidden']]))
-        par['Wo_init'] =  np.float32(np.random.uniform(-c, c, size = [par['n_input'], par['n_hidden']]))
-        par['Wc_init'] =  np.float32(np.random.uniform(-c, c, size = [par['n_input'], par['n_hidden']]))
-
-        par['Uf_init'] =  np.float32(np.random.uniform(-c, c, size = [par['n_hidden'], par['n_hidden']]))
-        par['Ui_init'] =  np.float32(np.random.uniform(-c, c, size = [par['n_hidden'], par['n_hidden']]))
-        par['Uo_init'] =  np.float32(np.random.uniform(-c, c, size = [par['n_hidden'], par['n_hidden']]))
-        par['Uc_init'] =  np.float32(np.random.uniform(-c, c, size = [par['n_hidden'], par['n_hidden']]))
-
-
-        par['bf_init'] = np.zeros((1, par['n_hidden']), dtype = np.float32)
-        par['bi_init'] = np.zeros((1, par['n_hidden']), dtype = np.float32)
-        par['bo_init'] = np.zeros((1, par['n_hidden']), dtype = np.float32)
-        par['bc_init'] = np.zeros((1, par['n_hidden']), dtype = np.float32)
-
-    ###
-    ### Setting up synaptic plasticity parameters
-    ###
-
-    """
-    0 = static
-    1 = facilitating
-    2 = depressing
-    """
-
-    par['synapse_type'] = np.zeros(par['n_hidden'], dtype=np.int8)
-
-    # only facilitating synapses
-    if par['synapse_config'] == 'stf':
-        par['synapse_type'] = np.ones(par['n_hidden'], dtype=np.int8)
-
-    # only depressing synapses
-    elif par['synapse_config'] == 'std':
-        par['synapse_type'] = 2*np.ones(par['n_hidden'], dtype=np.int8)
-
-    # even numbers facilitating, odd numbers depressing
-    elif par['synapse_config'] == 'std_stf':
-        par['synapse_type'] = 2*np.ones(par['n_hidden'], dtype=np.int8)
-        ind = range(1,par['n_hidden'],2)
-        #par['synapse_type'][par['ind_inh']] = 1
-        par['synapse_type'][ind] = 1
-
-    par['alpha_stf'] = np.ones((par['n_hidden'], 1), dtype=np.float32)
-    par['alpha_std'] = np.ones((par['n_hidden'], 1), dtype=np.float32)
-    par['U'] = np.ones((par['n_hidden'], 1), dtype=np.float32)
-
-    # initial synaptic values
-    par['syn_x_init'] = np.zeros((par['n_hidden'], par['batch_size']), dtype=np.float32)
-    par['syn_u_init'] = np.zeros((par['n_hidden'], par['batch_size']), dtype=np.float32)
-
-    for i in range(par['n_hidden']):
-        if par['synapse_type'][i] == 1:
-            par['alpha_stf'][i,0] = par['dt']/par['tau_slow']
-            par['alpha_std'][i,0] = par['dt']/par['tau_fast']
-            par['U'][i,0] = 0.15
-            par['syn_x_init'][i,:] = 1
-            par['syn_u_init'][i,:] = par['U'][i,0]
-
-        elif par['synapse_type'][i] == 2:
-            par['alpha_stf'][i,0] = par['dt']/par['tau_fast']
-            par['alpha_std'][i,0] = par['dt']/par['tau_slow']
-            par['U'][i,0] = 0.45
-            par['syn_x_init'][i,:] = 1
-            par['syn_u_init'][i,:] = par['U'][i,0]
-
-    par['alpha_stf'] = np.transpose(par['alpha_stf'])
-    par['alpha_std'] = np.transpose(par['alpha_std'])
-    par['U'] = np.transpose(par['U'])
-    par['syn_x_init'] = np.transpose(par['syn_x_init'])
-    par['syn_u_init'] = np.transpose(par['syn_u_init'])
+    ### Setting up LSTM weights and biases
+    c = 0.05
+    LSTM_var_names = ['Wf', 'Wi', 'Wo', 'Wc', 'W_pred', 'Uf', 'Ui', 'Uo', 'Uc', 'bf', 'bi', 'bo', 'bc', 'b_pred']
+    for name in LSTM_var_names:
+        par[name + '_init'] = []
+        for i in range(par['num_pred_cells']):
+            if name == 'W_pred':
+                par[name + '_init'].append(np.float32(np.random.uniform(-c, c, size = [par['n_hidden'][i], par['n_cell_input'][i]])))
+            elif name == 'b_pred':
+                par[name + '_init'].append(np.float32(np.random.uniform(-c, c, size = [1, par['n_cell_input'][i]])))
+            elif name.startswith('W'):
+                par[name + '_init'].append(np.float32(np.random.uniform(-c, c, size = [par['n_LSTM_input'][i], par['n_hidden'][i]])))
+            elif name.startswith('U'):
+                par[name + '_init'].append(np.float32(np.random.uniform(-c, c, size = [par['n_hidden'][i], par['n_hidden'][i]])))
+            elif name.startswith('b'):
+                par[name + '_init'].append(np.float32(np.random.uniform(-c, c, size = [1, par['n_hidden'][i]])))
 
 
 def gen_gating():
